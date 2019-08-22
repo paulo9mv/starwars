025efbd201749b21dde39729aff036ccf3816583
Paulo Viana <paulo@kiwicampus.com>
2019-08-22 05:01:56 -0500

    Added small text to MainScreen

---
+++ app/MainScreen.js
@@ -0,0 +1,11 @@
+import * as React from 'react'
+
+import {Text} from 'react-native'
+
+export default class MainScreen extends React.Component{
+    render(){
+        <Text>
+            MainScreen
+        </Text>
+    }
+}
